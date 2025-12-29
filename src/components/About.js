067d81afa88ee2b4edfe4d0b636c992cb1679b34
Daniel Mawuli Aloryi <aloryidaniel@gmail.com>
2025-12-29 06:54:33 +0000

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    const one = (
+      <p>
+        I am currently the <b>Founder and CEO</b> of
+        <a href="https://www.parallelafrica.com/"> Parallel</a>, where I lead the development
+        of Africa's software infrastructure across various industries. Previously, I was at
+        <a href="https://www.pwc.com/"> PwC</a> and{" "}
+        <a href="https://www.deloitte.com/"> Deloitte.</a> At the
+        same time, I am undertaking a part-time <b> Master's of Science</b> in{" "}
+        <b>Computer Science, Machine Learning</b>at{" "}
+        <a href="https://www.ox.ac.uk/about">The Georgia Institute of Technology, Georgia Tech</a>.
+      </p>
+    );
+    const two = (
+      <p>
+        Outside of work, I’m nerdy about history, love literary fiction,
+        and solve way too many math problems when I'm bored. Oh, I play Tennis too, well I'm learning.
+      </p>
+    );
+
+    const tech_stack = [
+      "Python",
+      "Typescript",
+      "React.js",
+      "Java",
+      "Javascript ES6+",
+      "C#",
+    ];
+
+    return (
+      <div id="about">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ about me</span>
+          </div>
+          <div className="about-content">
+            <div className="about-description">
+              {[one]}
+              {"Here are some technologies I have been working with:"}
+              <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <FadeInSection delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </FadeInSection>
+                  );
+                })}
+              </ul>
+              {[two]}
+            </div>
+            <div className="about-image">
+              <img alt="Daniel Aloryi" src={"/assets/me2.jpg"} />
+            </div>
+          </div>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default About;
